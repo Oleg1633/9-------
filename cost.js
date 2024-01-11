48e3e28dac77bdf3396bb33151c6b7177b1aff77
Oleg1633 <oleg345567@gmail.com>
2024-01-11 14:44:26 +0300

    стоимость

---
+++ cost.js
@@ -0,0 +1,7 @@
+//Пишем функцию, которая берет из поля "количество" и поля "цена" данные и выводит стоимость покупки.//
+function purchaseCost() {
+    var quantity=2;
+    var price=3;
+    var cost=quantity*price;
+    console.log(cost);
+}
