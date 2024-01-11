1b4a1ca74207af34efa65fd8454e36b3e49984cb
Oleg1633 <oleg345567@gmail.com>
2024-01-11 14:11:49 +0300

    1234

---
+++ 1234.js
@@ -0,0 +1,8 @@
+//Пишем функцию, которая это (1,2,3,4) делает по нажатию!//
+function numbers() {
+    var numbers = [1, 2, 3, 4];
+    for (var number of numbers) {  // перебор элементов
+        console.log(number);
+    }
+}
+
